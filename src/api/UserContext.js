900bc44d16360922a9a7dd2d5781fecc447e2984
Luiz Claudio <lcluizclaudio96@gmail.com>
2024-08-20 20:04:09 -0300

    feat: Add user authentication and local storage support

--- src/api/UserContext.js
+++ src/api/UserContext.js
@@ -1,13 +1,35 @@
 "use client"
-import React, { createContext, useState, useContext } from 'react';
+import { Api } from '@/services/api';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    username: '',
-    rol: '',
+  const [user, update] = useState(()=>{
+    if(typeof window === 'undefined'){
+      return null;
+    }
+    const storaged = localStorage.getItem('user');
+    if(storaged){
+      return JSON.parse(storaged);
+    }
+    return null;
   });
+
+  const setUser = (user) => {
+    localStorage.setItem('user', JSON.stringify(user));
+
+    console.log(user);
+
+    update(user);
+  };
+
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+    if (user) {
+      update(JSON.parse(user));
+    }
+  }, []);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
