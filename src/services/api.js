23a7fb4a32e6e72c9823e9e861f6557bbfc262a5
Luiz Claudio <lcluizclaudio96@gmail.com>
2024-08-21 20:04:06 -0300

    feat: Update API base URL to production server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const Api = axios.create({
-    baseURL : "http://localhost:2500"
+    baseURL : "https://api-rs-pquf.onrender.com/"
 })
 
 export const ApiBrasil = axios.create({
